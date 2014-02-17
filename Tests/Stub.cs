52367b115292f9ac16655810ea1f8cf01a118f40
Johan Dewe <johan@dewe.net>
2014-02-17 22:17:24 +0100

    Initial commit

---
+++ Tests/Stub.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tests
+{
+    public class Stub : IEnumerable<int>
+    {
+        public int CallsToGetEnumerator = 0;
+        public int IndexOfYield = 0;
+
+        public IEnumerator<int> GetEnumerator()
+        {
+            CallsToGetEnumerator++;
+            while (true)
+            {
+                yield return IndexOfYield++;
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
